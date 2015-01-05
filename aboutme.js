e704a2a07463bb396762b9e1a803c390a7d4c023
James OToole <jtotoole@gmail.com>
2015-01-05 10:43:01 -0500

    sections built out

--- aboutme.js
+++ aboutme.js
@@ -16,7 +16,6 @@ var server = http.createServer(function(request, response){
       fs.readFile("aboutmestyle.css", function (err, data){
         var data = data.toString();
         response.end(data);
-      });
     });
   }
   else if (path === "/words"){
@@ -24,29 +23,25 @@ var server = http.createServer(function(request, response){
       var data = data.toString();
       response.end(data);
     });
-  });
-}
+  }
   else if (path === "/code"){
     fs.readFile("code.html", function (err, data){
       var data = data.toString();
       response.end(data);
     });
-  });
-}
+  }
   else if (path === "/contact"){
     fs.readFile("contact.html", function (err, data){
       var data = data.toString();
       response.end(data);
     });
-  });
-}
+  }
   else if (path === "/mambo"){
     fs.readFile("mambo.html", function (err, data){
       var data = data.toString();
       response.end(data);
     });
-  });
-}
+  }
   else {
     var path = request.url;
     path = path.slice(1);
@@ -56,3 +51,15 @@ var server = http.createServer(function(request, response){
   }
 });
 server.listen(3000);
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+  var body = document.querySelector("body");
+  body.removeChild(button);
+  var video = document.createElement("iframe");
+  video.src = "http://www.youtube.com/embed/zYT2WVnwiLI?rel=0?autoplay=1";
+  video.width = '560';
+  video.height = '315';
+  document.body.appendChild(video);
+});
